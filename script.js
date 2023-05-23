f2e505ceeaa91a82c32cfe6cb551b50adf3a38fe
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-05-23 17:55:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-function decimalToBinary(decimal) {
-  // Check if the input is valid
-  if (decimal === 0) {
-    return '0';
-  } else if (decimal < 0 || !Number.isInteger(decimal)) {
-    return 'Invalid input. Please enter a positive integer.';
-  }
+const decimalToBinary = (num) => {
+  let binaryStr = num.toString(2);
+  return binaryStr;
+};
 
-  let binary = '';
-  while (decimal > 0) {
-    // Get the remainder when dividing by 2
-    const remainder = decimal % 2;
-    // Append the remainder to the binary string
-    binary = remainder + binary;
-    // Update the decimal by dividing it by 2
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
-}
-
-console.log(`Binary representation: ${binaryNumber}`);
+const num = parseInt(process.argv[2]);
+const binaryStr = decimalToBinary(num);
+console.log(binaryStr);
